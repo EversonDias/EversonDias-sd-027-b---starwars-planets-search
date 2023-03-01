fc5264f7ae20847c200eb0c545949696c6ce37f5
eversondias <eversondias@pop-os.localdomain>
2023-03-01 13:39:50 -0300

    create requeriment 5
    created test coverage

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,41 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('test da tela home', () => {
+  it('na tele a um input onde pode ser buscar pelo nome do planeta', () => {
+    render(<App />);
+    const getInput = screen.getByTestId('name-filter');
+    expect(getInput).toBeInTheDocument();
+  });
+
+  it('na tela tem que ter uma select com as opções de coluna', () => {
+    render(<App/>)
+    const getSelectColumn = screen.getByTestId('column-filter');
+    expect(getSelectColumn).toHaveLength(5)
+  })
+
+  it('na tela tem que ter uma select com as opções de comparador', () => {
+    render(<App/>)
+    const getSelectComparison = screen.getByTestId('comparison-filter');
+    expect(getSelectComparison).toHaveLength(3)
+  })
+
+  it('o header da tabela deve ter 13 colunas', async () => {
+    render(<App/>)
+    const response = await fetch('https://swapi.dev/api/planets');
+    await waitFor(() => {
+      const getColumnHeader = screen.getAllByRole('columnheader');
+      expect(getColumnHeader).toHaveLength(13);
+    })
+  })
+
+  it('o linhas da tabela deve ter 11', async () => {
+    render(<App/>)
+    const response = await fetch('https://swapi.dev/api/planets');
+    await waitFor(() => {
+      const getRow = screen.getAllByRole('row');
+      expect(getRow).toHaveLength(11);
+    })
+  })
+})
